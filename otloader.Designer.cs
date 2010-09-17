d638dc97917c2afce501d2f9cd5fc2e07f204b6a
Fighting Elf <fightingelf@users.sourceforge.net>
2010-09-17 02:54:45 +0000

    * Added an icon (probably not the best, but will somehow look for now) * Added minimize to tray: left click = activate, right click = close * Added "Favorite" button * Auto-favorite on patch works only when the patching goes successfully * Fixed a bug which caused application errors when trying to save settings on form close when the settings file couldn't be located * Removing a server from favorites erases the server/ port values * Port supports now only uint16 values, going to add some tooltip * Maximize button is now disabled for better effects * Sexy label for favorites :P
    Sim0ne: I've been playing a bit with designer, sorry if I've messed up something.

--- otloader.Designer.cs
+++ otloader.Designer.cs
@@ -28,99 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLoad = new System.Windows.Forms.Button();
-            this.editServer = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.editPort = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.checkBoxAutoAdd = new System.Windows.Forms.CheckBox();
-            this.listBoxServers = new System.Windows.Forms.ListBox();
-            this.SuspendLayout();
-            // 
-            // btnLoad
-            // 
-            this.btnLoad.Location = new System.Drawing.Point(168, 50);
-            this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(89, 23);
-            this.btnLoad.TabIndex = 2;
-            this.btnLoad.Text = "Patch";
-            this.btnLoad.UseVisualStyleBackColor = true;
-            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
-            // 
-            // editServer
-            // 
-            this.editServer.Location = new System.Drawing.Point(59, 24);
-            this.editServer.Name = "editServer";
-            this.editServer.Size = new System.Drawing.Size(198, 20);
-            this.editServer.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Server:";
-            // 
-            // editPort
-            // 
-            this.editPort.Location = new System.Drawing.Point(59, 50);
-            this.editPort.Name = "editPort";
-            this.editPort.Size = new System.Drawing.Size(56, 20);
-            this.editPort.TabIndex = 1;
-            this.editPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.editPort_KeyPress);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Port:";
-            // 
-            // checkBoxAutoAdd
-            // 
-            this.checkBoxAutoAdd.AutoSize = true;
-            this.checkBoxAutoAdd.Location = new System.Drawing.Point(134, 79);
-            this.checkBoxAutoAdd.Name = "checkBoxAutoAdd";
-            this.checkBoxAutoAdd.Size = new System.Drawing.Size(128, 17);
-            this.checkBoxAutoAdd.TabIndex = 3;
-            this.checkBoxAutoAdd.Text = "auto add new servers";
-            this.checkBoxAutoAdd.UseVisualStyleBackColor = true;
-            // 
-            // listBoxServers
-            // 
-            this.listBoxServers.FormattingEnabled = true;
-            this.listBoxServers.Location = new System.Drawing.Point(12, 102);
-            this.listBoxServers.Name = "listBoxServers";
-            this.listBoxServers.ScrollAlwaysVisible = true;
-            this.listBoxServers.Size = new System.Drawing.Size(245, 95);
-            this.listBoxServers.TabIndex = 4;
-            this.listBoxServers.SelectedValueChanged += new System.EventHandler(this.listBoxServers_SelectedValueChanged);
-            this.listBoxServers.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBoxServers_KeyUp);
-            // 
-            // FormOtloader
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(269, 214);
-            this.Controls.Add(this.listBoxServers);
-            this.Controls.Add(this.checkBoxAutoAdd);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.editPort);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.editServer);
-            this.Controls.Add(this.btnLoad);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "FormOtloader";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "otloader";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormOtloader_FormClosing);
-            this.Load += new System.EventHandler(this.FormOtloader_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOtloader));
+			this.btnLoad = new System.Windows.Forms.Button();
+			this.editServer = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.editPort = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBoxAutoAdd = new System.Windows.Forms.CheckBox();
+			this.listBoxServers = new System.Windows.Forms.ListBox();
+			this.btnFavorite = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.SuspendLayout();
+			// 
+			// btnLoad
+			// 
+			this.btnLoad.Location = new System.Drawing.Point(128, 50);
+			this.btnLoad.Name = "btnLoad";
+			this.btnLoad.Size = new System.Drawing.Size(60, 20);
+			this.btnLoad.TabIndex = 2;
+			this.btnLoad.Text = "Patch";
+			this.btnLoad.UseVisualStyleBackColor = true;
+			this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+			// 
+			// editServer
+			// 
+			this.editServer.Location = new System.Drawing.Point(56, 26);
+			this.editServer.Name = "editServer";
+			this.editServer.Size = new System.Drawing.Size(198, 20);
+			this.editServer.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 29);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Server:";
+			// 
+			// editPort
+			// 
+			this.editPort.Location = new System.Drawing.Point(56, 50);
+			this.editPort.Name = "editPort";
+			this.editPort.Size = new System.Drawing.Size(56, 20);
+			this.editPort.TabIndex = 1;
+			this.editPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.editPort_KeyPress);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(21, 53);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Port:";
+			// 
+			// checkBoxAutoAdd
+			// 
+			this.checkBoxAutoAdd.AutoSize = true;
+			this.checkBoxAutoAdd.Location = new System.Drawing.Point(122, 72);
+			this.checkBoxAutoAdd.Name = "checkBoxAutoAdd";
+			this.checkBoxAutoAdd.Size = new System.Drawing.Size(135, 17);
+			this.checkBoxAutoAdd.TabIndex = 3;
+			this.checkBoxAutoAdd.Text = "Auto-Favorite on Patch";
+			this.checkBoxAutoAdd.UseVisualStyleBackColor = true;
+			// 
+			// listBoxServers
+			// 
+			this.listBoxServers.FormattingEnabled = true;
+			this.listBoxServers.Location = new System.Drawing.Point(12, 92);
+			this.listBoxServers.Name = "listBoxServers";
+			this.listBoxServers.Size = new System.Drawing.Size(245, 95);
+			this.listBoxServers.TabIndex = 4;
+			this.listBoxServers.SelectedValueChanged += new System.EventHandler(this.listBoxServers_SelectedValueChanged);
+			this.listBoxServers.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBoxServers_KeyUp);
+			// 
+			// btnFavorite
+			// 
+			this.btnFavorite.Location = new System.Drawing.Point(194, 50);
+			this.btnFavorite.Name = "btnFavorite";
+			this.btnFavorite.Size = new System.Drawing.Size(60, 20);
+			this.btnFavorite.TabIndex = 5;
+			this.btnFavorite.Text = "Favorite";
+			this.btnFavorite.UseVisualStyleBackColor = true;
+			this.btnFavorite.Click += new System.EventHandler(this.btnFavorite_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(53, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Favorites:";
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "otloader";
+			this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+			// 
+			// FormOtloader
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(269, 199);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.btnFavorite);
+			this.Controls.Add(this.listBoxServers);
+			this.Controls.Add(this.checkBoxAutoAdd);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.editPort);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.editServer);
+			this.Controls.Add(this.btnLoad);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "FormOtloader";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "otloader";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormOtloader_FormClosing);
+			this.Load += new System.EventHandler(this.FormOtloader_Load);
+			this.SizeChanged += new System.EventHandler(this.FormOtloader_SizeChanged);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -133,6 +167,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox checkBoxAutoAdd;
         private System.Windows.Forms.ListBox listBoxServers;
+        private System.Windows.Forms.Button btnFavorite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- otloader.cs
+++ otloader.cs
@@ -20,16 +20,18 @@ namespace otloader
 	{
 		private otloader.Settings settings = new otloader.Settings();
 
-		string prevPatchedServer = "";
-		bool isClientPatched = false;
+		private System.Windows.Forms.Timer timer = new Timer();
+		private bool closing = true;
+		private Rectangle bounds;
 
-		List<string> clientServerList;
-		List<string> clientRSAKeys;
+		private string prevPatchedServer;
+		private bool isClientPatched = false;
 
-		string otservKey;
+		private List<string> clientServerList;
+		private List<string> clientRSAKeys;
+		private string otservKey;
 
 		private List<Server> storedServers;
-
 		public FormOtloader()
 		{
 			InitializeComponent();
@@ -43,39 +45,44 @@ namespace otloader
 			}
 
 			System.IO.Directory.SetCurrentDirectory(Application.StartupPath);
-			if(!System.IO.File.Exists(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), Settings.SettingFilename)))
+			if (!System.IO.File.Exists(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), Settings.SettingFilename)))
 			{
-				if(Properties.Settings.Default.SettingPath.Length > 0)
+				if (Properties.Settings.Default.SettingPath.Length > 0)
 				{
 					System.IO.Directory.SetCurrentDirectory(Properties.Settings.Default.SettingPath);
 				}
 			}
 
-			if(!settings.Load()){
+			if (!settings.Load())
+			{
 				FolderBrowserDialog dlg = new FolderBrowserDialog();
 				dlg.SelectedPath = Application.StartupPath;
 				dlg.ShowNewFolderButton = false;
-				dlg.Description = "Please locate the settings.xml, it should be located in the same folder as the program.";
+				dlg.Description = "Please locate the settings.xml file, it should be located in the same directory as the program.";
 
-				do{
+				do
+				{
 					DialogResult res = dlg.ShowDialog();
-					if(res != DialogResult.OK){
+					if (res != DialogResult.OK)
+					{
 						Application.Exit();
 					}
 
 					System.IO.Directory.SetCurrentDirectory(dlg.SelectedPath);
-				}while(!settings.Load());
-
+				}
+				while (!settings.Load());
 				Properties.Settings.Default.SettingPath = dlg.SelectedPath;
 			}
 
 			clientServerList = settings.GetClientServerList();
 			clientRSAKeys = settings.GetRSAKeys();
 			otservKey = settings.OtservRSAKey;
+
 			checkBoxAutoAdd.Checked = settings.AutoAddServer;
 			storedServers = settings.GetServerList();
 			UpdateServerList();
 
+			bounds = this.Bounds;
 			if (listBoxServers.Items.Count > 0)
 			{
 				listBoxServers.SelectedIndex = 0;
@@ -84,18 +91,46 @@ namespace otloader
 
 		private void FormOtloader_FormClosing(object sender, FormClosingEventArgs e)
 		{
-			settings.UpdateServerList(storedServers);
-			settings.AutoAddServer = checkBoxAutoAdd.Checked;
-			settings.Save();
+			if (!closing)
+			{
+				e.Cancel = true;
+				timer.Tick += new EventHandler(timer_Tick);
+				timer.Interval = 10;
+				timer.Enabled = true;
+			}
+			else if(e.CloseReason != CloseReason.ApplicationExitCall)
+			{
+				settings.UpdateServerList(storedServers);
+				settings.AutoAddServer = checkBoxAutoAdd.Checked;
+				settings.Save();
 
-			Properties.Settings.Default.Location = base.DesktopBounds.Location;
-			Properties.Settings.Default.Save();
+				Properties.Settings.Default.Location = base.DesktopBounds.Location;
+				Properties.Settings.Default.Save();
+			}
 		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			Rectangle workingArea = Screen.GetWorkingArea(this.Bounds);
+			this.Bounds = new Rectangle(
+				Math.Max(workingArea.Width - this.Width, this.Left + 10),
+				Math.Max(workingArea.Height - this.Height, this.Top + 10),
+				Math.Max(0, this.Width - 10),
+				Math.Max(0, this.Height - 10));
+
+			if (this.Bounds.Bottom >= workingArea.Bottom)
+			{
+				timer.Tick -= new EventHandler(timer_Tick);
+				timer.Enabled = false;
+
+				closing = true;
+                this.Close();
+            }
+        }
 
 		private void UpdateServerList()
 		{
 			listBoxServers.Items.Clear();
-
 			foreach (Server server in storedServers)
 			{
 				listBoxServers.Items.Add(server.name + ":" + server.port);
@@ -104,7 +139,7 @@ namespace otloader
 
 		private PatchResult patchClient()
 		{
-			if (!Utils.IsClientRunning())
+			if (!Utils.IsClientRunning()) 
 			{
 				return PatchResult.CouldNotFindClient;
 			}
@@ -151,30 +186,7 @@ namespace otloader
 
 		private void btnLoad_Click(object sender, EventArgs e)
 		{
-			if (checkBoxAutoAdd.Checked)
-			{
-				bool isStored = false;
-				foreach (Server server in storedServers)
-				{
-					if (server.name == editServer.Text && server.port.ToString() == editPort.Text)
-					{
-						isStored = true;
-						break;
-					}
-				}
-
-				if(!isStored)
-				{
-					Server server = new Server();
-					server.name = editServer.Text;
-					server.port = Convert.ToUInt16(editPort.Text);
-					storedServers.Add(server);
-					UpdateServerList();
-				}
-			}
-
 			PatchResult result = patchClient();
-
 			switch (result)
 			{
 				case PatchResult.CouldNotFindClient: MessageBox.Show("Could not find client!"); break;
@@ -182,26 +194,71 @@ namespace otloader
 				case PatchResult.CouldNotPatchServerList: MessageBox.Show("Could not patch server list!"); break;
 				case PatchResult.Success:
 					{
+						if (checkBoxAutoAdd.Checked)
+						{
+							btnFavorite_Click(sender, e);
+						}
+
 						prevPatchedServer = editServer.Text;
 						isClientPatched = true;
 						break;
 					}
 				default: break;
 			}
+
+
+		}
+
+		private void btnFavorite_Click(object sender, EventArgs e)
+		{
+			bool isStored = false;
+			foreach (Server server in storedServers)
+			{
+				if (server.name == editServer.Text && server.port.ToString() == editPort.Text)
+				{
+					isStored = true;
+					break;
+				}
+			}
+
+			if (!isStored)
+			{
+				Server server = new Server();
+				server.name = editServer.Text;
+				server.port = Convert.ToUInt16(editPort.Text);
+
+				storedServers.Add(server);
+				UpdateServerList();
+			}
 		}
 
 		private void editPort_KeyPress(object sender, KeyPressEventArgs e)
 		{
-			if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+			if (!Char.IsControl(e.KeyChar))
 			{
-				e.Handled = true;
+				if (Char.IsDigit(e.KeyChar))
+				{
+					try
+					{
+						UInt16.Parse(editPort.Text + e.KeyChar);
+					}
+					catch
+					{
+						//TODO: some tooltip in here
+						e.Handled = true;
+					}
+				}
+				else
+				{
+					e.Handled = true;
+				}
 			}
 		}
 
 		private void listBoxServers_SelectedValueChanged(object sender, EventArgs e)
 		{
 			if (listBoxServers.SelectedIndex != -1 &&
-			    listBoxServers.SelectedIndex < storedServers.Count)
+				listBoxServers.SelectedIndex < storedServers.Count)
 			{
 				Server server = storedServers[listBoxServers.SelectedIndex];
 				editServer.Text = server.name;
@@ -211,13 +268,72 @@ namespace otloader
 
 		private void listBoxServers_KeyUp(object sender, KeyEventArgs e)
 		{
-			if ((Keys)e.KeyValue == Keys.Delete)
+			if (listBoxServers.SelectedIndex != -1)
 			{
-				if (listBoxServers.SelectedIndex != -1)
+				switch ((Keys)e.KeyValue)
 				{
-					storedServers.RemoveAt(listBoxServers.SelectedIndex);
-					UpdateServerList();
+					case Keys.Delete:
+						{
+							Server server = storedServers[listBoxServers.SelectedIndex];
+							storedServers.RemoveAt(listBoxServers.SelectedIndex);
+							UpdateServerList();
+
+							editServer.Text = "";
+							editPort.Text = "";
+							break;
+						}
+
+					case Keys.Enter:
+						{
+							//TODO
+							break;
+						}
+
+					default: break;
 				}
+			}
+		}
+
+		private void FormOtloader_SizeChanged(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Minimized)
+			{
+				this.ShowInTaskbar = false;
+				this.notifyIcon.Visible = true;
+
+				closing = false;
+				this.Close();
+			}
+		}
+
+		private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.ShowInTaskbar = true;
+				this.notifyIcon.Visible = false;
+
+				//remove the timer event
+				timer.Tick -= new EventHandler(timer_Tick);
+				timer.Enabled = false;
+
+				this.Bounds = bounds; //bring original bounds
+				closing = true;
+
+				this.WindowState = FormWindowState.Normal;
+				this.Activate();
+			}
+			else if (e.Button == MouseButtons.Right)
+			{
+				//remove the timer event
+				timer.Tick -= new EventHandler(timer_Tick);
+				timer.Enabled = false;
+
+				this.Bounds = bounds; //bring original bounds
+				closing = true;
+
+				this.WindowState = FormWindowState.Normal; //need the window back to save a proper location
+				this.Close();
 			}
 		}
 	}
