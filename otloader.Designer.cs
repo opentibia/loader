3dfb6ca486164cbed60e377a6c85d0c73004603b
Johannes Rosenqvist <xeroc81@gmail.com>
2010-09-18 17:57:25 +0000

    * Tabify

--- otloader.Designer.cs
+++ otloader.Designer.cs
@@ -1,33 +1,33 @@
 ﻿namespace otloader
 {
-    partial class FormOtloader
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class FormOtloader
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			this.components = new System.ComponentModel.Container();
 			this.btnLoad = new System.Windows.Forms.Button();
 			this.editServer = new System.Windows.Forms.TextBox();
@@ -137,7 +137,7 @@
 			// statusStrip1
 			// 
 			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
+			this.toolStripStatusLabel1});
 			this.statusStrip1.Location = new System.Drawing.Point(0, 193);
 			this.statusStrip1.Name = "statusStrip1";
 			this.statusStrip1.Size = new System.Drawing.Size(269, 22);
@@ -178,23 +178,23 @@
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.Button btnLoad;
-        private System.Windows.Forms.TextBox editServer;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox editPort;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckBox checkBoxAutoAdd;
-        private System.Windows.Forms.ListBox listBoxServers;
-        private System.Windows.Forms.Button btnFavorite;
-        private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnLoad;
+		private System.Windows.Forms.TextBox editServer;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox editPort;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBoxAutoAdd;
+		private System.Windows.Forms.ListBox listBoxServers;
+		private System.Windows.Forms.Button btnFavorite;
+		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.NotifyIcon notifyIcon;
 		private System.Windows.Forms.ToolTip toolTip1;
 		private System.Windows.Forms.StatusStrip statusStrip1;
 		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-    }
+	}
 }
 
